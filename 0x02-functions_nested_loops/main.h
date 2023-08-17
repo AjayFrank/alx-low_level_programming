88493b702183f7bfb5954897a4b76950059c3cfb
AjayFrank <ebukaajaegbo@gmail.com>
2023-08-16 19:26:26 -0700

    5-sign.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,3 @@
-int _putchar(char c);
-
 void print_alphabet(void);
 
 void print_alphabet_x10(void);
@@ -23,3 +21,5 @@ int add(int, int);
 void print_to_98(int n);
 
 void print_times_table(int n);
+
+int _putchar(int n);
