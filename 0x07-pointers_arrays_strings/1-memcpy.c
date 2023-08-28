36ce95b4032369bc9ed2fa663d5ad3ca31f4155a
AjayFrank <ebukaajaegbo@gmail.com>
2023-08-28 16:16:06 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _memcpy - Entry point
+ * @dest: input
+ * @src: input
+ * @n: input
+ * Return: Pointer to the destination buffer 'dest'
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	dest[i] = src[i];
+
+	return (dest);
+}
